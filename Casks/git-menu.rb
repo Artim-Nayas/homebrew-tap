334ca5299cc7c12d6293648b7a4f8f0472f3d53b
Sayan Mitra <sayan.mitra@getsimplifin.com>
2026-06-23 13:49:56 +0530

    Update git-menu to 0.2.0

---
+++ Casks/git-menu.rb
@@ -0,0 +1,29 @@
+cask "git-menu" do
+  version "0.2.0"
+  sha256 "0e6aeaf47ada59ad204fa0731038bb4f3ba42ed97220a5eaf5544987a1d441a4"
+
+  url "https://github.com/Artim-Nayas/git-menu/releases/download/v#{version}/Git-Menu-#{version}-arm64.dmg",
+      verified: "github.com/Artim-Nayas/git-menu/"
+  name "Git Menu"
+  desc "Menu-bar app for GitHub PRs, reviews, inbox and Actions, powered by the gh CLI"
+  homepage "https://github.com/Artim-Nayas/git-menu"
+
+  depends_on macos: :big_sur
+  depends_on arch: :arm64
+
+  app "Git Menu.app"
+
+  # The app is ad-hoc signed but not notarized (free, no Apple Developer account),
+  # so clear the download quarantine on install to let it launch normally.
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "#{appdir}/Git Menu.app"]
+  end
+
+  zap trash: [
+    "~/Library/Application Support/Git Menu",
+    "~/Library/Logs/Git Menu",
+    "~/Library/Preferences/com.artimnayas.gitmenu.plist",
+    "~/Library/Saved Application State/com.artimnayas.gitmenu.savedState",
+  ]
+end
